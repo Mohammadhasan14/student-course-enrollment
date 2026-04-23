cbf7a70973ab9706c18edb0ab490425ea86bf075
hasan <mohdhasan7867214@gmail.com>
2026-04-23 23:17:45 +0530

    fixed the issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-const SquareGrid = () => {
-  const totalSquares = 60;
+const totalBoxes = 10
+const sampleBoxes = Array(totalBoxes).fill(false)
 
-  // false = black, true = white
-  const [squares, setSquares] = useState(Array(totalSquares).fill(false));
-  const [index, setIndex] = useState(0);
+function App() {
+  const [squareBoxes, setSquareBoxes] = useState(sampleBoxes)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSquares((prev) => {
-        const newSquares = [...prev];
-
-        // If all squares are white → reset
-        if (index >= totalSquares) {
-          setIndex(0);
-          return Array(totalSquares).fill(false);
+    let interval = setInterval(() => {
+      setSquareBoxes(prev => {
+        if (totalBoxes === currentIndex) {
+          setCurrentIndex(0)
+          return sampleBoxes
         }
 
-        // Turn next square white
-        newSquares[index] = true;
-        return newSquares;
-      });
+        const boxes = [...prev]
 
-      setIndex((prev) => prev + 1);
-    }, 1000);
+        boxes[currentIndex] = true
+        return boxes
+      })
+      setCurrentIndex(pr => pr + 1)
+    }, 1000)
 
-    return () => clearInterval(interval);
-  }, [index]);
+    return () => {
+      clearInterval(interval)
+    }
+  }, [currentIndex])
 
   return (
-    <div style={gridStyle}>
-      {squares.map((isWhite, i) => (
-        <div
-          key={i}
-          style={{
-            ...squareStyle,
-            backgroundColor: isWhite ? "white" : "black",
-          }}
-        />
-      ))}
+    <div style={styles.parentDiv}>
+      {squareBoxes.map((data, index) => {
+        return (
+          <div key={index} style={{ ...(styles.squareBoxes), ...({ backgroundColor: data ? '#fff' : '#000' }) }} ></div>
+        )
+      })}
     </div>
-  );
-};
+  )
+}
 
-const gridStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(10, 40px)",
-  gap: "5px",
-};
+const styles = {
+  squareBoxes: {
+    width: '26px',
+    height: '26px',
+    border: '1px solid #000'
+  },
+  parentDiv: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(10, 40px)',
+    gap: '4px',
+    padding: '20px'
+  }
+}
 
-const squareStyle = {
-  width: "40px",
-  height: "40px",
-  border: "1px solid #ccc",
-};
-
-export default SquareGrid;
+export default App;
